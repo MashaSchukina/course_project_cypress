6b8f32634a86c36ff51c2cff2f187318b6be9a12
Shcukina Mariia <mashaschukina49@gmail.com>
2026-06-29 20:17:57 +0300

    mocha configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,16 @@ require('dotenv').config();
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    html: false,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
